185948451361134807ab4960a03d2a7009589139
AniTei <115176414+AniTei@users.noreply.github.com>
2023-10-12 11:05:32 +0200

    register

---
+++ js/register.js
@@ -0,0 +1,44 @@
+const registerForm = document.querySelector("#register-form");
+
+//got 201 with jsonplaceholder
+//add real api
+const apiBaseUrl = "https://api.noroff.dev/api/v1";
+const apiRegisterEndpoint = "/social/auth/register";
+const url = apiBaseUrl + apiRegisterEndpoint;
+console.log(url);
+
+
+
+function registerNewUser(userInput) {
+
+    const optionsForRegistering = {
+        method: 'POST',
+        body: JSON.stringify(userInput),
+    };
+
+    fetch(url, optionsForRegistering);
+   
+    console.log(userInput);
+
+}
+
+function collectRegisterInput(event) {
+    event.preventDefault();
+
+    const usernamePutIn = document.querySelector('#register-username').value;
+    const emailPutIn = document.querySelector('#register-email').value;
+    const passwordPutIn = document.querySelector('#register-password').value;
+
+    const informationPutIn = {
+        name: usernamePutIn,
+        email: emailPutIn,
+        password: passwordPutIn,
+    }
+
+    registerNewUser(informationPutIn);
+
+}
+
+registerForm.addEventListener("submit", collectRegisterInput);
+
+
