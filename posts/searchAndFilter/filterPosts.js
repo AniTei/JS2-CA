60eae8ae4f8b70dd76a554bd850e33a7b2aaed89
AniTei <115176414+AniTei@users.noreply.github.com>
2023-10-15 22:56:00 +0200

    typo

--- posts/searchAndFilter/filterPosts.js
+++ posts/searchAndFilter/filterPosts.js
@@ -1,15 +1,9 @@
 const postsContainer = document.querySelector('.posts-container');
 
 postsContainer.innerHTML = `<div><p>Trollolol</p></div>`;
-//vurder å bruke noe annet enn innerHTML
+//vurder å bruke noe annet enn innerHTML?
 
 console.log("hei feed, nå skal jeg get posts fra appppppiiii")
-
-
-
-
-// GET posts(how many) to inhabit feed
-// display them
 
 
 const apiBaseUrl = "https://api.noroff.dev/api/v1";
@@ -17,13 +11,10 @@ const apiFilterEndpoint = "/social/posts?_tag="
 const tag = "ja";
 const active = "&_active=true";
 const url = apiBaseUrl + apiFilterEndpoint + tag + active;
-
 console.log(url);
 
 const token = localStorage.getItem('token');
 console.log(token);
-
-
 
 async function getPosts() {
 
@@ -39,40 +30,25 @@ async function getPosts() {
 
         const respons = await fetch(url, options);
         const json = await respons.json();
-
         console.log(json);
 
         for (let i = 0; i < json.length; i++) {
 
             postsContainer.innerHTML += `<div class="post">
-    
-                    <p>Today I noticed...</p>
                     <p class="post-title">${json[i].title}</p>
                     <p class="post-body">${json[i].body}</p>
-
                     <img class="post-media" src="${json[i].media}">
-
                     <div class="open">
-                        
                         <a href="post-opened.html?id=${json[i].id}" >
                         open
                         </a>
                     </div>
                 </div>`;
         }
-
-
     } catch (error) {
         console.log(error);
     }
-
-
-
-
-    //loop 10 first????
-    //include id so it can be passed to seperate page
 };
-
 
 getPosts();
 
