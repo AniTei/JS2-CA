9bac014bb9b723e057b06cf9d64cff8570f61c51
AniTei <115176414+AniTei@users.noreply.github.com>
2023-12-05 09:05:24 +0100

    adds search functionality to feed

--- posts/postsInFeed.js
+++ posts/postsInFeed.js
@@ -1,12 +1,6 @@
+
 const postsContainer = document.querySelector('.posts-container');
-
-//postsContainer.innerHTML = `<div><p>Trollolol</p></div>`;
-//vurder å bruke noe annet enn innerHTML
-
-
-// GET posts to inhabit feed
-// display them
-
+const searchBar = document.querySelector('#search');
 
 const apiBaseUrl = "https://api.noroff.dev/api/v1";
 const apiPostsEndpoint = "/social/posts";
@@ -17,46 +11,100 @@ console.log(url);
 const token = localStorage.getItem('token');
 console.log(token);
 
+const options = {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+    },
+}
 
-async function getPosts() {
 
-    const options = {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`,
-        },
-    }
+function buildPosts(data) {
 
-    try {
+    postsContainer.innerHTML = "";
 
-        const respons = await fetch(url, options);
-        const json = await respons.json();
+    for (let i = 0; i < data.length; i++) {
 
-        console.log(json);
-
-        for (let i = 0; i < json.length; i++) {
-
-            postsContainer.innerHTML += `<div class="post">
-                    <p class="post-title">${json[i].title}</p>
-                    <p class="post-body">${json[i].body}</p>
-                    <img class="post-media" src="${json[i].media}">
-
-                    <p>tags: ${json[i].tags} </p>
+        postsContainer.innerHTML += `<div class="post">
+                    <p class="post-title">${data[i].title}</p>
+                    <p class="post-body">${data[i].body}</p>
+                    <img class="post-media" src="${data[i].media}">
+                    <p>tags: ${data[i].tags} </p>
                     <div class="open">
-                        <a href="post-opened.html?id=${json[i].id}">
+                        <a href="post-opened.html?id=${data[i].id}">
                         open
                         </a>
                     </div>
                 </div>`;
+    }
+};
+
+async function getData() {
+
+    try {
+        const respons = await fetch(url, options);
+        const data = await respons.json();
+
+        console.log("data recieved from API:", data);
+
+        /* return data*/
+
+        //read about callback functions ??
+
+        /// how do i get something out of the function so i can display it?
+
+
+        // it's better if one function does one thing, and then just call that function inside another function :)
+        buildPosts(data);
+
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+getData();
+
+
+
+
+async function searchData() {
+
+    try {
+        const respons = await fetch(url, options);
+        const data = await respons.json();
+
+        buildPosts(data);
+
+    
+
+        // make on key up into seperare function? 
+
+        searchBar.onkeyup = function collectSearchInput(event) {
+            const conditionFromInput = event.target.value.toLowerCase();
+
+            console.log("event:", conditionFromInput);
+        
+            const filteredPosts = data.filter((post) => {
+                if (post.title.includes(conditionFromInput)){
+
+                    // remember lower case, trim, and maybe include rather than starts with
+                    // oops something about making posts also lower case?
+                    // next make them show up in feed :))
+                    // first just make it work <3 then make it better <3
+                    return true;
+                }
+            });
+
+            buildPosts(filteredPosts);
+
+            // error in line 67, filteredPosts is undefined?? But it works?
+
         }
     } catch (error) {
         console.log(error);
     }
 };
 
-getPosts();
-
-
-
-
+searchData();
