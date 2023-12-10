f38c0afd8067b1cd340efe5ace1ae856994af0e4
AniTei <115176414+AniTei@users.noreply.github.com>
2023-12-10 22:59:22 +0100

    removes console logs

--- RegisterAndLogin/login.mjs
+++ RegisterAndLogin/login.mjs
@@ -1,10 +1,8 @@
 
-import {apiBaseUrl} from '../api/url.mjs';
+import { apiBaseUrl } from '../api/url.mjs';
 
 const apiLoginEndpoint = "/social/auth/login";
 const url = apiBaseUrl + apiLoginEndpoint;
-
-console.log(url);
 
 const loginForm = document.querySelector("#login-form");
 loginForm.addEventListener("submit", collectLoginInput);
@@ -46,24 +44,12 @@ async function collectLoginInput(event) {
 
     try {
 
-        console.log(informationPutIn)
-
         await LoginUser(informationPutIn);
 
     } catch (error) {
         console.log(error);
     }
 }
-
-
-
-
-// add async/await
-// maybe move the url info?
-// put fetch in variable
-// what does returning the json do? 
-
-
 
 
 
@@ -101,28 +87,16 @@ async function LoginUser(userInput) {
         },
     };
 
-console.log(userInput);
-
 
     const response = await fetch(url, optionsForLogin);
     const json = await response.json();
 
-    console.log(response);
-    console.log(json.accessToken);
-
-
-
     localStorage.setItem('token', json.accessToken);
 
-    console.log ("this token:",json.accessToken);
 
-    const token = localStorage.getItem('token');
-
-    console.log("from local storage" ,token);
-
-  /*   localStorage.clear();
-
-    return; */
+    /*    const token = localStorage.getItem('token');
+    localStorage.clear();
+      return; */
 
     if (response.ok) {
 
@@ -132,16 +106,5 @@ console.log(userInput);
         takeUserToFeed();
     }
 }
-
-
-
-
-// what to do with access token
-// something abt putting it in local storage 
-// if ok take user to feed
-// but first
-// store accessitem in order to retrieve it on diff pages?
-
-// on success take user to feed
 
 
