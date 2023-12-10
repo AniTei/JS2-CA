93a1048f5a93252215bad3f0048dcdad3d356c83
AniTei <115176414+AniTei@users.noreply.github.com>
2023-12-10 22:11:03 +0100

    adds JSDocs

--- RegisterAndLogin/register.mjs
+++ RegisterAndLogin/register.mjs
@@ -7,6 +7,31 @@ console.log(url);
 const publishForm = document.querySelector("#register-form");
 publishForm.addEventListener("submit", collectLoginInput);
 
+
+
+
+/**This function selects input from form on submit, 
+ * created an objedt
+ * and passes that object into another function that makes use of it
+ * 
+ * @param {*} event 
+ * @example
+ * ```js
+ * // prevents refresh
+    event.preventDefault();
+
+// collects form input and turns it into an object
+    const form = event.target;
+    const formData = new FormData(form);
+    const informationPutIn = Object.fromEntries(formData.entries());
+    
+    try {
+// passes new object into function to make use of it
+        await registerUser(informationPutIn)
+
+ * ```
+ */
+
 async function collectLoginInput(event) {
     event.preventDefault();
 
@@ -15,10 +40,10 @@ async function collectLoginInput(event) {
     const informationPutIn = Object.fromEntries(formData.entries());
 
     console.log(informationPutIn);
-    /* return; */
+    
 
     try {
-        
+
         await registerUser(informationPutIn)
 
     } catch (error) {
@@ -35,7 +60,26 @@ async function collectLoginInput(event) {
 // what does returning the json do? 
 
 
+/**Function makes use of an object to create a user in the api
+ * 
+ * @param {object} userInput 
+ * @example 
+ * ```js
+ * //options contains object to create user
+    const optionsForRegistering = {
+        method: 'POST',
+        body: JSON.stringify(userInput),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
+// makes contact with api
+    const response = await fetch(url, optionsForRegistering);
+    const json = await response.json();
 
+    console.log(response);
+ * ```
+ */
 async function registerUser(userInput) {
 
     console.log(userInput);
